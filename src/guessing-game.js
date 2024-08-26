0c673d9db60cb1c0b6908585633d29ccb8704ff8
RomaaaK <zimpsonzya@gmail.com>
2024-08-26 19:13:19 +0300

    feat: implement guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
     constructor() {}
 
-    setRange(min, max) {
+    min = 0;
+    max = 0;
 
+    setRange(min, max) {
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.ceil((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess();
     }
 }
 
